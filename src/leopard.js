4cd7350499f9b462a8203d47c81c648c12bfef21
longjia <longjiarun@qq.com>
2016-08-30 18:15:30 +0800

    publish 1.0.0

--- src/leopard.js
+++ src/leopard.js
@@ -1,8 +1,12 @@
 !(function() {
     var _window = window
 
+    var AJAX_EVENT = 0,
+        RENDER_EVENT = 1
+
     var ajax = _window.ajax || _window.Zepto.ajax || _window.jQuery.ajax,
-        caches = {}
+        ajaxCaches = {},
+        renderCaches = {}
 
     var run = function(stack, context, args) {
         stack && stack.forEach(function(callback) {
@@ -10,51 +14,56 @@
         })
     }
 
-    var proxy = function(opts) {
+    var handler = function(opts, caches, context) {
         var id = opts.id,
             cache = caches[id],
             success = opts.success,
             error = opts.error
 
-        if (cache) {
-            var sargs = cache.sargs,
-                eargs = cache.eargs
+        if (cache && cache.initial) return true
 
-            if (sargs || eargs) {
-                sargs && success && success.apply(null, sargs)
-                eargs && error && error.apply(null, eargs)
-            } else {
-                cache.sstack.push(success)
-                cache.estack.push(error)
-            }
-            return
-        }
+        if (id != null) {
 
-        if (id) {
-            cache = {
-                sstack: [success],
-                estack: [error]
-            }
+            !cache && (cache = {
+                sstack: [],
+                estack: []
+            })
+
+            cache.sstack.push(success)
+            cache.estack.push(error)
+
+            cache.initial = true
 
             opts.success = function() {
                 var args = arguments
+                cache.completed = true
+                cache.succeed = true
                 cache.sargs = args
                 run(cache.sstack, this, args)
             }
 
             opts.error = function() {
                 var args = arguments
+                cache.completed = true
+                cache.succeed = false
                 cache.eargs = args
                 run(cache.estack, this, args)
             }
 
             caches[id] = cache
         }
-
-        return ajax && ajax(opts)
+        return false
     }
 
-    /*var render = function(opts) {
+    var proxy = function(opts) {
+        if (handler(opts, ajaxCaches, this)) return this
+        ajax && ajax(opts)
+        return this
+    }
+
+    var render = function(opts) {
+        if (handler(opts, renderCaches, this)) return this
+
         var template = opts.template,
             success = opts.success,
             error = opts.error
@@ -67,7 +76,8 @@
         } catch (e) {
             error && error(e)
         }
-    }*/
+        return this
+    }
 
     var paramsRegxp = /([^=&]+)(=([^&#]*))?/g
     var getQuerystring = function() {
@@ -81,9 +91,47 @@
         return params
     }
 
+    var listener = function(opts) {
+        var id = opts.id,
+            event = opts.event,
+            caches = event === AJAX_EVENT ? ajaxCaches : event === RENDER_EVENT ? renderCaches : null,
+            cache = caches && caches[id],
+            success = opts.success,
+            error = opts.error
+
+        if (cache) {
+            var sargs = cache.sargs,
+                eargs = cache.eargs,
+                succeed = cache.succeed
+
+            if (cache.completed) {
+                succeed && success && success.apply(null, sargs)
+                !succeed && error && error.apply(null, eargs)
+            } else {
+                cache.sstack.push(success)
+                cache.estack.push(error)
+            }
+
+            return this
+        }
+
+        if (id != null && caches) {
+            caches[id] = {
+                sstack: [success],
+                estack: [error]
+            }
+        }
+
+        return this
+    }
+
     var leopard = {
+        AJAX_EVENT: AJAX_EVENT,
+        RENDER_EVENT: RENDER_EVENT,
         ajax: proxy,
-        getQuerystring: getQuerystring
+        render: render,
+        getQuerystring: getQuerystring,
+        listener: listener
     }
 
     if (typeof module !== 'undefined' && module.exports) {
